1cc4134af7fc2c02c2a1d9a95bb2b97c00715955
Abhijit Dasgupta <aikidasgupta@gmail.com>
2020-05-12 06:37:29 -0400

    index

---
+++ docs/index.md
@@ -0,0 +1,33 @@
+---
+jupyter:
+  jupytext:
+    formats: ipynb,md,py:percent
+    text_representation:
+      extension: .md
+      format_name: markdown
+      format_version: '1.2'
+      jupytext_version: 1.4.2
+  kernelspec:
+    display_name: Python 3
+    language: python
+    name: python3
+---
+
+# An Introduction to Data Science using Python
+
+## Instructor: Abhijit Dasgupta, PhD
+
+## Schedule
+
+## Day One
+
+### Morning
+
++ [Introduction to the workshop](Introduction.html)
++ [A Python Primer](python_primer.md)
++ [Python Tools for Data Science](python_tools_ds.md)
+
+### Afternoon
+
++ [An Introduction to `pandas` for data ingestion and munging](python_pandas.md)
+
---
+++ docs/index.py
@@ -0,0 +1,34 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,md,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.4.2
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # An Introduction to Data Science using Python
+#
+# ## Instructor: Abhijit Dasgupta, PhD
+#
+# ## Schedule
+#
+# ## Day One
+#
+# ### Morning
+#
+# + [Introduction to the workshop](Introduction.html)
+# + [A Python Primer](python_primer.md)
+# + [Python Tools for Data Science](python_tools_ds.md)
+#
+# ### Afternoon
+#
+# + [An Introduction to `pandas` for data ingestion and munging](python_pandas.md)
+#
